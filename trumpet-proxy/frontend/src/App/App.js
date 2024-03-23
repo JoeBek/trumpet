c46bbaf2f051f262744816c5d5fd2dcf30c05039
Isaac Thompson <shufflesninja@gmail.com>
2024-03-23 14:01:29 -0400

    Add frontend

---
+++ trumpet-proxy/frontend/src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Trumpet Prosthetic</h1>
+        <h2>Upload music</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
